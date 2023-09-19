cba24de42e9350799b3e165df0bc6e9ac6529290
gabrielapolo1 <141746587+gabrielapolo1@users.noreply.github.com>
2023-09-18 23:18:22 -0300

    ListaLigada.cpp

--- ListaLigada/ListaLigada/ListaLigada.cpp
+++ ListaLigada/ListaLigada/ListaLigada.cpp
@@ -70,7 +70,7 @@ void menu()
 
 void inicializar()
 {
-	// se a lista j� possuir elementos
+	// se a lista já possuir elementos
 // libera a memoria ocupada
 	NO* aux = primeiro;
 	while (aux != NULL) {
@@ -116,38 +116,148 @@ void inserirElemento()
 {
 	// aloca memoria dinamicamente para o novo elemento
 	NO* novo = (NO*)malloc(sizeof(NO));
+	NO* aux, *ant = primeiro;
+	int inserir;
+
 	if (novo == NULL)
 	{
 		return;
 	}
 
 	cout << "Digite o elemento: ";
-	cin >> novo->valor;
-	novo->prox = NULL;
+	cin >> inserir;
 
 	if (primeiro == NULL)
 	{
+
+		novo->valor = inserir;
+		novo->prox = NULL;
 		primeiro = novo;
+
 	}
-	else
-	{
-		// procura o final da lista
-		NO* aux = primeiro;
-		while (aux->prox != NULL) {
-			aux = aux->prox;
+
+	else if(inserir == primeiro->valor) {
+			cout << "Valor digitado ja existente" << endl;
+			return;
+	}
+	else if(inserir < primeiro->valor){
+		
+		novo->valor = inserir;
+		novo->prox = primeiro;
+			
+		primeiro = novo;
+
+	}
+	else{
+
+		aux = primeiro->prox;
+
+			while (aux) {
+
+				if (inserir == aux->valor) {
+					cout << "Valor digitado ja existente" << endl;
+					return;
+				}
+				if (inserir < aux->valor) {
+
+					novo->valor = inserir;
+					novo->prox = aux;
+					ant->prox = novo;
+					return;
+				}
+				ant = aux;
+				aux = aux->prox;
+			}
+			
+
+			novo->valor = inserir;
+			novo->prox = NULL;
+			ant->prox = novo;
 		}
-		aux->prox = novo;
-	}
 }
 
 void excluirElemento()
 {
 
+	if (primeiro == NULL) {
+		cout << "Lista vazia" << endl;
+	}
+	else {
+		NO* aux, * ant;
+		int deletar;
+
+		aux = primeiro;
+
+		cout << "Digite o numero que deseja excluir: \n" << endl;
+		cin >> deletar;
+
+		if (aux) {
+			if (aux->valor == deletar) {
+				primeiro = aux->prox;
+				free(aux);
+				cout << "Valor excluido" << endl;
+				return;
+			}
+			else if (deletar < aux->valor) {
+				cout << "Valor não encontrado" << endl;
+				return;
+			}
+			else {
+
+				ant = aux;
+				aux = aux->prox;
+			}
+
+			while (aux) {
+
+				if (aux->valor == deletar) {
+					ant->prox = aux->prox;
+					free(aux);
+					cout << "Valor excluido" << endl;
+					return;
+				}
+				else if (aux->valor > deletar) {
+					cout << "Valor não encontrado" << endl;
+					return;
+				}
+				else {
+					ant = aux;
+					aux = aux->prox;
+				}
+
+			}
+
+		}
+
+		}
 }
 
 void buscarElemento()
 {
+	NO* aux = primeiro;
+	int busca;
+	if (primeiro == NULL) {
+		cout << "Lista vazia" << endl;
+	}
+	else {
+		cout << "Digite o valor de busca: \n" << endl;
+		cin >> busca;
+
+		while (aux) {
+			if (aux->valor == busca) {
+				cout << "Valor encontrado" << endl;
+				return;
+			}
+			else if (aux->valor < busca) {
+				cout << "Valor nao encontrado" << endl;
+				return;
+			}
+			else {
+				aux = aux->prox;
+
+			}
+		}
+	}
 
 }
-
 
